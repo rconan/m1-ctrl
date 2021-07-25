6ba49867c9e38c5b3367cbd5a991aff7a2290f21
Rod Conan <rconan@gmto.org>
2021-07-25 13:14:06 -0700

    ADD ACTUATORS FORCE LOOP CONTROLLER

---
+++ src/actuators/segment7/M1SA_F_Control_CS_types.h
@@ -0,0 +1,28 @@
+/*
+ * File: M1SA_F_Control_CS_types.h
+ *
+ * Code generated for Simulink model 'M1SA_F_Control_CS'.
+ *
+ * Model version                  : 1.762
+ * Simulink Coder version         : 9.0 (R2018b) 24-May-2018
+ * C/C++ source code generated on : Fri Jul 23 14:06:06 2021
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Linux 64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_M1SA_F_Control_CS_types_h_
+#define RTW_HEADER_M1SA_F_Control_CS_types_h_
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_M1SA_F_Control_CS_T RT_MODEL_M1SA_F_Control_CS_T;
+
+#endif                                 /* RTW_HEADER_M1SA_F_Control_CS_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
