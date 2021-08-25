4897079685c6941fd302b54ee39e59863bb02b72
Rod Conan <rconan@gmto.org>
2021-08-25 14:05:39 -0700

    ADD MISSING DATA SOURCE C FILE FOR HP DYNAMICS

--- build.rs
+++ build.rs
@@ -18,6 +18,7 @@ fn main() {
         .compile("m1_hp_load_cells");
     cc::Build::new()
         .file("src/hp_dynamics/M1_HP_Dyn.c")
+        .file("src/hp_dynamics/M1_HP_Dyn_data.c")
         .compile("m1_hp_dynamics");
     cc::Build::new()
         .file("src/local_controller/M1LocalControl.c")
